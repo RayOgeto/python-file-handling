2d524bd2a09adcf693dd3226374a55e64219aaf4
Ogeto <128529806+RayOgeto@users.noreply.github.com>
2024-05-04 18:04:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 
-with open("my_file.txt", "w") as file:
-    file.write("sleepy head\n")
-    file.write("C'mon wake up 34.\n")
-    file.write("whoooo!\n")
+try:
+    with open("my_file.txt", 'w') as file:
+        file.write("sleepy head \n")
+        file.write("C'mon wake up!!\n")
+        file.write("Nambasss7\n")
+except FileNotFoundError:
+    print("File not found.")
+except PermissionError:
+    print("Permission denied.")
+finally:
+    print("File creation completed.")
 
 
-with open("my_file.txt", "a") as file:
-    file.write("append mode.\n")
-    file.write("98765 r.\n")
-    file.write("easy!\n")
+try:
+    with open("my_file.txt", 'r') as file:
+        print("Contents of my_file.txt:")
+        for line in file:
+            print(line.strip())
+except FileNotFoundError:
+    print("File not found.")
+except PermissionError:
+    print("Permission denied.")
+finally:
+    print("File reading completed.")
 
-with open("my_file.txt", "r") as file:
-   
-    contents = file.read()
-
-    print(contents)
+try:
+    with open("my_file.txt", 'a') as file:
+        file.write(" append mode\n")
+        file.write("randomm 0\n")
+        file.write("come closer man\n")
+except FileNotFoundError:
+    print("File not found.")
+except PermissionError:
+    print("Permission denied.")
+finally:
+    print("File appending completed.")
